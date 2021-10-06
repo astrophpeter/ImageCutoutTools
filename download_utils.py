59bd6d6e1ae2059524a0315df7487e55b814864d
astrophpeter <petermcgill94@gmail.com>
2021-10-06 10:01:28 -0700

    completed 2MASS download function

--- download_utils.py
+++ download_utils.py
@@ -3,40 +3,31 @@
 
 from astropy.io import fits
 import requests
-import xml.etree.ElementTree as ET
 from astropy.io.votable import parse
+from astropy.coordinates import SkyCoord
 import io
 
-ra = 150.0
-dec = -30
-size=0.001
-
-base_url = 'http://irsa.ipac.caltech.edu/cgi-bin/2MASS/IM/nph-im_sia?'
-query_url = base_url + f'{ra},{dec}&SIZE={size}'
-
-
-xml = requests.get(url=query_url).content
-print(xml)
-votable = parse(io.BytesIO(xml))
-print(votable.resources)
-table = votable.resources[0].tables[0]
-
-# only take the first epoch if J H K fits files, (:3)
-fits_mask = table.array['format'] == 'image/fits'
-fits_url = table.array['download'][fits_mask][:3]
-bands = table.array['band'][fits_mask][:3]
-#print({band: url for band, url in zip(bands,fits_url)})
-
-
-#print(irsa)
-
-def twomass_cutout(ra, dec, size=0.001):
+def twomass_cutouts(position, image_size=0.0001):
     """
+    Get the fits cutouts from the 2MASS survey
 
-    :param ra: Right acension [Deg]
-    :param dec: declinatoin [Deg
-    :param size: size of cutout
-    :return: fits image
+    :param position: On Sky position of the centre of the cutout
+    :param image_size: Size of the cutout
+    :return: fits images of J, H, and K band images
     """
+    base_url = 'http://irsa.ipac.caltech.edu/cgi-bin/2MASS/IM/nph-im_sia?'
+    query_url = base_url + f'POS={position.ra.degree},{position.dec.degree}&SIZE={image_size}'
 
-    return 0.0
+    xml = requests.get(url=query_url).content
+    votable = parse(io.BytesIO(xml))
+    table = votable.resources[0].tables[0]
+
+    # only take the first epoch if J H K fits files, (:3)
+    # TODO: grab the image with the longest exposure time
+    fits_mask = table.array['format'] == 'image/fits'
+    fits_url = table.array['download'][fits_mask][:3]
+    bands = table.array['band'][fits_mask][:3]
+    return {band: fits.open(url) for band, url in zip(bands, fits_url)}
+
+position = SkyCoord(ra=130.0, dec=-30.0, unit='degree')
+print(twomass_cutouts(position))
