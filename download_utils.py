b2f372e36f2ebb7e929c6fe254fa6c833a7994e4
astrophpeter <petermcgill94@gmail.com>
2021-10-07 11:34:25 -0700

    added hips2fits, looks like a homogenius way to get cutout fits, not sure on the width ang height parameters though

--- download_utils.py
+++ download_utils.py
@@ -2,9 +2,11 @@
 # utiliy functions to get image cutouts from various surveys
 
 from astropy.io import fits
+import astropy.units as u
 import requests
 from astropy.io.votable import parse
 from astropy.coordinates import SkyCoord
+from astroquery.hips2fits import hips2fits
 import io
 
 def twomass_cutouts(position, image_size=0.0001):
@@ -29,14 +31,39 @@ def twomass_cutouts(position, image_size=0.0001):
     bands = table.array['band'][fits_mask][:3]
     return {band: fits.open(url) for band, url in zip(bands, fits_url)}
 
-def wise_cutouts(position, image_size=0.0001):
+def cutout(position=None, survey=None, fov=0.001, width=1000, height=1000):
     """
-    Get the fits cutouts from the wise survey
+    Get image cutout for a survey in a particular band
 
-    :param position: On Sky position of the centre of the cutout
-    :param image_size: Size of the cutout
-    :return:
+    :param position: On Sky position of the center of the cutout
+    :param survey: Survey and band string, for allowed options see https://aladin.u-strasbg.fr/hips/list
+    :param image_fov: Field of view in decimal degrees
+    :param image_width: image width in pixels
+    :param image_height: image height in pixels
+    :return: fits image
     """
+    fits = hips2fits.query(hips=survey,
+                           ra=position.ra,
+                           dec=position.dec,
+                           width=width, height=height, fov=fov * (u.deg),
+                           projection='TAN',
+                           format='fits')
+    return fits
 
-position = SkyCoord(ra=130.0, dec=-30.0, unit='degree')
-print(twomass_cutouts(position))
+def all_cutouts(position):
+
+    names = {'2MASS/': ['H', 'J', 'K'],
+             'WISE/':['W1', 'W2', 'W3', 'W4'],
+             'GALEXGR6/AIS/': ['FUV','NUV'],
+             'PanSTARRS/DR1/': ['g','r', 'i', 'z', 'y'],
+             'SDSS9/' : ['i', 'r', 'u', 'z'],
+             'DES-DR1/' : ['Y', 'g', 'r', 'i', 'z']             }
+
+    for survey in names:
+        filters = names[survey]
+        for filter in filters:
+            print(survey + filter)
+            print(cutout(position=position, survey=survey + filter))
+
+position = SkyCoord(ra=130, dec=-30, unit='degree')
+all_cutouts(position)
