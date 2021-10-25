fa875b262e44229d0b91b6ea3e38ca648e7dbe57
astrophpeter <petermcgill94@gmail.com>
2021-10-25 16:20:45 -0700

    added aperture photometry construction

--- image_processing.py
+++ image_processing.py
@@ -7,9 +7,14 @@ from astropy.coordinates import SkyCoord
 from photutils.background import Background2D, MedianBackground
 from astropy.wcs import WCS
 import numpy as np
+import yaml
+from astropy.wcs.utils import proj_plane_pixel_scales
+
+with open("survey_metadata.yml", "r") as stream:
+    survey_metadata = yaml.safe_load(stream)
 
 supernova_position = SkyCoord(ra=188.5126408, dec=7.6991489, unit='deg')
-image = cutout(position=supernova_position, survey='CDS/P/2MASS/H')
+image = cutout(position=supernova_position, survey=survey_metadata['PanSTARRS_z']['hips_id'],fov=0.3, width=300, height=300)
 data = image[0].data
 threshold = detect_threshold(data, nsigma=2.)
 
@@ -50,11 +55,52 @@ def match_sources_to_host(image, source_catalog, host_position):
     host_x_pixel, host_y_pixel = world_coordinate_system.world_to_pixel(host_position)
     source_x_pixels, source_y_pixels = source_catalog.xcentroid, source_catalog.ycentroid
     closest_source_index = np.argmin(np.hypot(host_x_pixel - source_x_pixels, host_y_pixel - source_y_pixels))
-
     return source_catalog[closest_source_index]
 
+def elliptical_sky_aperture(source_cat, world_coordinate_system, r=3.0):
+    """Constructs elliptical sky aperture object
+    """
+    center = (source_cat.xcentroid, source_cat.ycentroid)
+    semi_major_axis = source_cat.semimajor_sigma.value * r
+    semi_minor_axis = source_cat.semiminor_sigma.value * r
+    orientation_angle = source_cat.orientation.to(u.rad).value
+    pixel_aperture = EllipticalAperture(center, semi_major_axis, semi_minor_axis, theta=orientation_angle)
+    return pixel_aperture.to_sky(world_coordinate_system)
 
-#cat = build_source_catalog(image)
-#host = match_sources_to_host(image, cat, supernova_position)
 
-#print(host.kron_flux)
+def find_largest_aperture(host_position, survey_images):
+    """Find the largest fitted elliptical aperture from a list of images
+
+    :param host_position:
+    :param surveys:
+    :return:
+    """
+    for image in survey_images:
+        pass
+
+
+
+
+
+import astropy.units as u
+from photutils.aperture import EllipticalAperture
+import matplotlib.pyplot as plt
+from matplotlib.colors import PowerNorm
+
+cat = build_source_catalog(image)
+#cat = match_sources_to_host(image, cat, supernova_position)
+#position = (cat.xcentroid, cat.ycentroid)
+
+#r = 1.0  # approximate isophotal extent
+#a = cat.semimajor_sigma.value * r
+#b = cat.semiminor_sigma.value * r
+#theta = cat.orientation.to(u.rad).value
+#apertures = EllipticalAperture(position, a, b, theta=theta)
+
+#plt.imshow(data, origin='lower', cmap='gray', interpolation='nearest', norm=PowerNorm(0.5))
+#apertures.plot(color='#d62728')
+#plt.show()
+host = match_sources_to_host(image, cat, supernova_position)
+wcs = WCS(image[0].header)
+print(wcs.pixel_scale_matrix[0,0] / wcs.proj_plane_pixel_scales()[0].value)
+#print(proj_plane_pixel_scales(wcs))
